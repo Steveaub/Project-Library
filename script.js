c0a55d39eab559ecac3b9a073e80906d4990e541
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-10-05 22:35:41 -0400

    Implemented Form Submission for Adding Books
    - Added functionality to toggle form visibility.
    - Inserted a submit button within the form for book data submission.
    - Established event listener on form to handle submission and prevent
      default behavior.
    - Created and added new `Book` objects to `myLibrary` array upon form
      submission.
    - Updated display dynamically to showcase newly added books.

--- script.js
+++ script.js
@@ -1,105 +1,70 @@
-// Function to add a new book from user input into an array.
-// Function to loop through array, displaying each book in a table or card.
-// "NEW BOOK" button to open a form for book details: author, title, pages, read status, etc.
-// Button on each book display to remove the book.
-// Associate DOM elements with book objects via data-attribute for array index.
-// Button on each book display to toggle read status.
-// Create function on Book prototype to toggle a book's read status.
+document.addEventListener('DOMContentLoaded', (event) => {
+    const myLibrary = [];
 
-const myLibrary = [];
-console.log(myLibrary);
-
-
-function myLibraryDisplay (){
-
-    let bookContainer = document.getElementById('book-container');
-    bookContainer.textContent = ""; 
-
-    for( let i in myLibrary ) {
-        let bookDisplay = document.createElement("div")
-        let book = myLibrary[i]
-        
-        bookDisplay.textContent = `${book.title} by ${book.author}, ${book.pages} pages. ${book.readStatus()}`;
-        bookContainer.appendChild(bookDisplay);
+    function Book(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 
-   
-}
+    Book.prototype.readStatus = function() {
+        if (this.read === false) {
+            return "I have not read this book.";
+        } else {
+            return "I have read this book.";
+        }
+    };
 
-function Book(title, author, pages, read) {
-  this.title = title;
+    function myLibraryDisplay() {
+        const bookContainer = document.getElementById("book-container");
+        bookContainer.textContent = "";
 
-  this.author = author;
+        for (let i in myLibrary) {
+            const bookDisplay = document.createElement("div");
+            const book = myLibrary[i];
 
-  this.pages = pages;
-  this.read = read;
-}
-
-Book.prototype.readStatus = function () {
-  if (this.read === false) {
-    return "I have not read this book.";
-  } else {
-    return "I have  read this book.";
-  }
-};
-
-let button = document.getElementById("new-book");
-button.addEventListener("click", function(){
-    myLibraryDisplay ()
-})
-
-function toggleForm() {
-    
-    let form = document.getElementById('hiddenForm');
-    if (form.style.display === 'none') {
-        form.style.display = 'block'; 
-    } else {
-        form.style.display = 'none'; 
+            bookDisplay.textContent = `${book.title} by ${book.author}, ${book.pages} pages. ${book.readStatus()}`;
+            bookContainer.appendChild(bookDisplay);
+        }
     }
-}
 
+    const form = document.getElementById("hiddenForm");
+    form.style.display = "none"; // Hide form on page load
 
-let form = document.getElementById('hiddenForm');
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    console.log("Form Submitted!")
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let pages = document.getElementById('pages').value; 
-    let read = document.getElementById('myCheckbox').checked;
-    
-    let newBook = new Book(title, author, pages, read);
-    console.log(newBook);
-    myLibrary.push(newBook);
-    console.log(myLibrary);
-    myLibraryDisplay(); 
-    
-    form.reset(); 
-    form.style.display = 'none'; 
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pages = document.getElementById("pages").value;
+        const read = document.getElementById("myCheckbox").checked;
+
+        const newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook);
+
+        myLibraryDisplay();
+        form.reset();
+        form.style.display = "none"; // Hide form after submission
+    });
+
+    document.getElementById("new-book").addEventListener("click", function() {
+        if (form.style.display === "none") {
+            form.style.display = "block"; // Show form
+        } else {
+            form.style.display = "none"; // Hide form
+        }
+    });
+
+    // Sample books
+    const firstBook = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 320, false);
+    myLibrary.push(firstBook);
+
+    const secondBook = new Book("Dune", "Frank Herbert", 896, false);
+    myLibrary.push(secondBook);
+
+    const thirdBook = new Book("Foundation", "Isaac Asimov", 255, false);
+    myLibrary.push(thirdBook);
+
+    myLibraryDisplay(); // Display initial books
 });
-
-
-
-
-const firstBook = new Book(
-  "Harry Potter and the Sorcerer's Stone",
-  "jk rowling",
-  320,
-  false
-);
-myLibrary.push(firstBook);
-
-
-
-const secondBook = new Book("Dune", "Frank Herbert", 896, false);
-myLibrary.push(secondBook);
-
-
-const thirdBook = new Book(" Foundation", "Isaac Asimov", 255, false);
-
-myLibrary.push(thirdBook);
-
-console.log(firstBook);
-console.log(secondBook);
-console.log(thirdBook);
